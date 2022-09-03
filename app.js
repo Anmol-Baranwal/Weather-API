0fb92bb08bd205e39b642b28a4c64a0d0150cbe0
Anmol Baranwal <anmolbaranwal119@gmail.com>
2022-09-03 19:18:01 +0530

    added AQI category with few changes

--- app.js
+++ app.js
@@ -26,10 +26,12 @@ app.get("/", function(req,res){
 });
 
 const apiKey= process.env.OPEN_WEATHER_API_KEY
+const cageAPIKey= process.env.OPEN_CAGE_API_KEY
 
-var lat;
-var lon;
+var latCopy;
+var lonCopy;
 var queryInput;
+var nationCode;
 
 app.get('/weather',function(req,res){
     res.sendFile(__dirname+'/weather.html');
@@ -43,30 +45,33 @@ app.post("/air-quality", function(req,res){
         request("https://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid="+ apiKey +"", function(error, response, body){
 
             // to get the json response in the console
-            queryInput= req.body.zipCode;
+            queryInput= req.body.addressUser;
             var dataAPI=JSON.parse(response.body);
             console.log(dataAPI);
         });
         
-        const countryCode="IN";
-        const urlCoordinate= "https://api.openweathermap.org/geo/1.0/zip?zip="+ queryInput +","+ countryCode +"&appid="+ apiKey +"";
+        // const urlCoordinate= "https://api.opencagedata.com/geocode/v1/json?q="+ queryInput +"&key="+ cageAPIKey +"";
         
-        https.get(urlCoordinate, function(response){
-            response.on("data", function(data){
-                const zipData= JSON.parse(data);
-                lat= zipData.lat; // the latitude of the city
-                lon= zipData.lon;
-                queryInput= zipData.name;
-            });
-            res.end();
-        });
-
+        // https.get(urlCoordinate, function(response){
+        //     response.on("data", function(data){
+        //         const addressData= JSON.parse(data);
+        //         lat= addressData.results[0].geometry.lat; // the latitude of the city
+        //         lon= addressData.results[0].geometry.lng;
+        //         latCopy=lat;
+        //         lonCopy=lon;
+        //         queryInput= addressData.results[0].components.city;
+        //         nationCode=addressData.results[0].components['ISO_3166-1_alpha-2'];
+        //     });
+        // });
+        
         // 1. We can get the zip code from the user & retrieve the country code from that location thus making air pollution api call of openweathermap
         // 2. We can get the address from the user thus getting the more accurate coordinates and making the api call of openweathermap
         // Earlier I was making a api call to openweathermap from the zip code entered by user & country code by opencage, then making final api call with the coordinates to openweathermap
         // But I think the opencage api would be sufficient for the thing so I am making the changes, In case you want to see then see commit history
-       
-        const url= "https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat="+ lat +"&lon="+ lon +"&appid="+ apiKey +"";
+        console.log(lonCopy);
+        
+        // const url= "https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat="+ latCopy +"&lon="+ lonCopy +"&appid="+ apiKey +"";
+        const url= "https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=25.3923&lon=82.5533&appid="+ apiKey +"";
 
         https.get(url, function(response){
             console.log(response.statusCode); // to get the status code in the terminal
@@ -112,11 +117,76 @@ app.post("/air-quality", function(req,res){
                 else if(airQualityIndex==4)   airDesc="Poor :  People should moderately reduce outdoor activities, they may develop heart & respiratory problems.";
                 else if(airQualityIndex==5)   airDesc="Very Poor : Serious health implications";
 
+                // AQI Category & Health Breakpoints
+                // co level category
+                var coDesc="";
+                if(co>=0 && co<=1)  coDesc="Good!";
+                else if(co>=1.1 && co<=2)   coDesc="Satisfactory!";
+                else if(co>=2.1 && co<=10)   coDesc="Moderate!";  
+                else if(co>10 && co<=17)   coDesc="Poor!";  
+                else if(co>17 && co<=34)   coDesc="Severe!";  
+                else if(co>34)   coDesc="Harzadous!";  
+
+                // no2 level category
+                var no2Desc="";
+                if(no2>=0 && no2<=40)  no2Desc="Good!";
+                else if(no2>=41 && no2<=80)   no2Desc="Satisfactory!";
+                else if(no2>=81 && no2<=180)   no2Desc="Moderate!";  
+                else if(no2>=181 && no2<=280)   no2Desc="Poor!";  
+                else if(no2>=281 && no2<=400)   no2Desc="Severe!";  
+                else if(no2>400)   no2Desc="Harzadous!";
+
+                // o3 level category
+                var o3Desc="";
+                if(o3>=0 && o3<=50)  o3Desc="Good!";
+                else if(o3>=51 && o3<=100)   o3Desc="Satisfactory!";
+                else if(o3>=101 && o3<=168)   o3Desc="Moderate!";  
+                else if(o3>=169 && o3<=208)   o3Desc="Poor!";  
+                else if(o3>=209 && o3<=748)   o3Desc="Severe!";  
+                else if(o3>748)   no2Desc="Harzadous!";
+
+                // so2 level category
+                var so2Desc="";
+                if(so2>=0 && so2<=40)  so2Desc="Good!";
+                else if(o3>=41 && o3<=80)   so2Desc="Satisfactory!";
+                else if(o3>=81 && o3<=380)   so2Desc="Moderate!";  
+                else if(o3>=381 && o3<=800)   so2Desc="Poor!";  
+                else if(o3>=801 && o3<=1600)   so2Desc="Severe!";  
+                else if(o3>1600)   no2Desc="Harzadous!";
+
+                // pm2.5 level category
+                var pm2Desc="";
+                if(pm2_5>=0 && pm2_5<=30)  pm2Desc="Good!";
+                else if(pm2_5>=31 && pm2_5<=60)   pm2Desc="Satisfactory!";
+                else if(pm2_5>=61 && o3<=90)   pm2Desc="Moderate!";  
+                else if(pm2_5>=91 && o3<=120)   pm2Desc="Poor!";  
+                else if(pm2_5>=121 && o3<=250)   pm2Desc="Severe!";  
+                else if(pm2_5>250)   no2Desc="Harzadous!";
+
+                // pm10 level category
+                var pm10Desc="";
+                if(pm10>=0 && pm10<=50)  pm10Desc="Good!";
+                else if(pm10>=51 && pm10<=100)   pm10Desc="Satisfactory!";
+                else if(pm10>=101 && pm10<=250)   pm10Desc="Moderate!";  
+                else if(pm10>=251 && pm10<=350)   pm10Desc="Poor!";  
+                else if(pm10>=351 && pm10<=430)   pm10Desc="Severe!";  
+                else if(pm10>430)   pm10Desc="Harzadous!";
                 
+                // nh3 level category
+                var nh3Desc="";
+                if(nh3>=0 && nh3<=200)  nh3Desc="Good!";
+                else if(nh3>=201 && nh3<=400)   nh3Desc="Satisfactory!";
+                else if(nh3>=401 && nh3<=800)   nh3Desc="Moderate!";  
+                else if(nh3>=801 && co<=1200)   nh3Desc="Poor!";  
+                else if(nh3>1200 && co<=1800)   nh3Desc="Severe!";  
+                else if(nh3>1800)   coDesc="Harzadous!";
+
                 res.render('output-airQuality',{city: query, airQlt: airQualityIndex, date: dt, coLevel: co,
                     noLevel: no, latitute: lat, longitude: lon, no2Level: no2, o3Level: o3, so2Level: so2,
                     fineParticles: pm2_5, coarseParticles: pm10, nh3Level: nh3, vH: verticalHemisphere, 
-                    hH: horizontalHemisphere, airDescription: airDesc});   // sending all info related to different fields to the output-airQuality.ejs
+                    hH: horizontalHemisphere, airDescription: airDesc, coDescription: coDesc, no2Description: no2Desc,
+                    o3Description: o3Desc, so2Description: so2Desc, pm2Description: pm2Desc, pm10Description: pm10Desc,
+                    nh3Description: nh3Desc});   // sending all info related to different fields to the output-airQuality.ejs
 
                 res.end();
 
